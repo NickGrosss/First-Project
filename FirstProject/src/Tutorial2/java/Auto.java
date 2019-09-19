dd7cfea6b10489cfcef261bbe47da48245233020
Nick <N.Gross@sw-machines.de>
2019-09-19 09:01:04 +0200

    change

---
+++ FirstProject/src/Tutorial2/java/Auto.java
@@ -0,0 +1,9 @@
+package Tutorial2.java;
+
+public class Auto {
+	public static void methode(){
+		for(int i=1;i<10;i++) {
+			System.out.println(i);
+		}
+}
+}
---
+++ FirstProject/src/Tutorial2/java/Tutorial2.java
@@ -0,0 +1,23 @@
+package Tutorial2.java;
+
+public class Tutorial2 {
+
+	public static void main(String[] args) {
+	Auto objekt = new Auto();
+	
+	// TODO Auto-generated method stub
+
+	System.out.println("Hi");
+	objekt.methode();
+
+}
+
+	}
+ 
+
+
+
+	
+
+
+ 
